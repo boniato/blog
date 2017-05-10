dfd789ebf05c15d482ae513c5a49134ec454ff9c
boniato <teegeegle@gmail.com>
2017-05-10 23:27:59 +0900

    add a comment
    add a comment

--- src/test/java/com/boniato/SpringBootCleanBlogApplicationTests.java
+++ src/test/java/com/boniato/SpringBootCleanBlogApplicationTests.java
@@ -5,6 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * Created by Lee on 2017. 5. 5..
+ */
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootCleanBlogApplicationTests {
